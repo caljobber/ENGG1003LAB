08fc73bfbea9d6ed520f5c7a700fbabeff0b57db
caljobber <Callum.jobber@gmail.com>
2020-03-31 20:42:11 +1100

    Update fractal.c
    now works correctly mapping the values with RGB colour.

--- fractal.c
+++ fractal.c
@@ -2,13 +2,13 @@
 #include <math.h>
 #include <stdlib.h>
 
-int red(int h);
-int green(int h);
-int blue(int h);
+int red(float h);
+int green(float h);
+int blue(float h);
 int main(){
-    double x0=-2.0, y0=1.0, xn= x0, yn=y0, xt;
-    int i=0, maxi=1000;//iteration counter&max iterations
-    int height=4000, width=8000, heightc=0, widthc=0;
+    double x0=-2, y0=1.0, xn= x0, yn=y0, xt;
+    int i=0, maxi=100;//iteration counter&max iterations
+    int height=4200, width=8400, heightc=0, widthc=0;
     double incw=(double)(-2.0*x0)/(double)width;//increment for width
     double dech=(double)2*y0/(double)height; //decrement for height
     //height & width of image, c is counter for each pixel drawn
@@ -16,16 +16,22 @@ int main(){
     output = fopen("fractest.ppm", "w");
     fprintf(output, "P3\n%d %d\n255\n", width, height);
     for(heightc=0; heightc<height; heightc++){
-        x0=-2.0;
+
+        x0=-2;
         widthc=0;
+
         for(widthc=0; widthc<width; widthc++){
+
             xn=x0, yn=y0;//begin mandelbrot test sequence
-            for(i=0; (i<maxi && xn*xn+yn*yn<=4); i++){
-            xt=((xn*xn-yn*yn)+x0);
-            yn=(2*xn*yn+y0);
-            xn=xt;
+
+            for(i=0; (i<maxi && xn*xn+yn*yn<=2); i++){
+                xt=((xn*xn-yn*yn)+x0);
+                yn=(2*xn*yn+y0);
+                xn=xt;
             }
-            int h=(360*i/maxi), R, G, B;
+            float h=360*(float)i/(float)maxi;
+            int R, G, B;
+
             R= red(h);
             G= green(h);
             B= blue(h);
@@ -42,10 +48,10 @@ int main(){
     fclose(output);
     return 0;
 }
-int red(int h){
+int red(float h){
 
-    int X =(1-(abs((h/60)%2-1)));
-    int R;
+    float X =(1-(fabs(fmodf((h/60), 2)-1)));
+    int R=0;
     if((h>=0) && (h<60))
         R=255;
     else if((h>=60) && (h<120))
@@ -60,9 +66,9 @@ return R;
 }
 
 
-int green(int h){
-    int X =(1-(abs((h/60)%2-1)));
-    int G;
+int green(float h){
+    float X =(1-(fabs(fmodf((h/60), 2)-1)));
+    int G=0;
     if((h>=0) && (h<60))
         G=X*255;
     else if((h>=60) && (h<180))
@@ -75,9 +81,9 @@ return G;
 }
 
 
-int blue(int h){
-    int X =(1-(abs((h/60)%2-1)));
-    int B;
+int blue(float h){
+    float X =(1-(fabs(fmodf((h/60), 2)-1)));
+    int B=0;
     if((h>=0) && (h<120))
         B=0;
     else if((h>=120) && (h<180))
